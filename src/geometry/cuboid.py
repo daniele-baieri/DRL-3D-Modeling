4cb152a20bcf1a393a64964fdf6463efe8bce067
daniele-baieri <daniele.baieri@gmail.com>
2020-05-19 17:37:53 +0200

    add geometric primitives

---
+++ src/geometry/cuboid.py
@@ -0,0 +1,19 @@
+import torch
+from primitive import Primitive
+
+
+class Cuboid(Primitive):
+
+    def __init__(self, v: torch.FloatTensor):
+        """
+        @param v: FloatTensor of shape (2,3) containing the 
+        coordinates of vertices V and V'
+        """
+        super.__init__()
+        assert v.shape[0] == 2 and v.shape[1] == 3
+        self.__vert = v
+        # self.__points = 2
+
+    def slide(self, vertex: int, axis: int, amount: float):
+        self.__vert[vertex,axis] += amount
+
---
+++ src/geometry/primitive.py
@@ -0,0 +1,12 @@
+import typing
+
+
+class Primitive:
+
+    def slide(self, vertex: int, axis: int, amount: float):
+        """
+        @param vertex: the vertex to slide along axis 'axis'
+        @param axis: the axis along which to slide vertex 'vertex'
+        @param amount: the additive factor of which to slide 'vertex' on 'axis'
+        """
+        pass
